1f8e93bb7f51618963ce767123861aeedb781f97
PedroLorenzen <107435301+PedroLorenzen@users.noreply.github.com>
2022-11-28 10:52:35 +0100

    Update person

--- Person.java
+++ Person.java
@@ -1,27 +1,37 @@
 public class Person
 {
-    String name = Uganda;
+    String name = "Uganda";
     int age = -99;
+    int kontingent = -100;
     boolean medlemsskab;
 
-    public void whatMember() {
+    public int whatMember() 
+    {
         if (medlemsskab == true) {
             if (age < 18)
             {
                 // indsæt medlemsskab ungdomssvømmere 1000kr i arraylist.
+                System.out.println("hello");
+                return kontingent = 1000;
             }
-            else if (age >= 18 && < 60)
+            else if (age >= 17 && age < 60)
             {
                 // indsæt medlemsskab seniorsvømmere 1600kr i arraylist
+                System.out.println("hello");
+                return kontingent = 1600;
             }
             else if (age > 60)
             {
                 // // indsæt medlemsskab pensionist 25% af seniortakst 1600kr (1200kr) i arraylist
+                System.out.println("hello");
+                return kontingent = 1200;
             }
         }
         else if (medlemsskab == false)
         {
-            // kontingent == 500kr.
+            return kontingent = 500;
+            //System.out.println("hello");
         }
+        return kontingent;
     }
 }
