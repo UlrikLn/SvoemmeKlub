4d695864ec69a24fcd83e6329567716a610fcd61
PedroLorenzen <107435301+PedroLorenzen@users.noreply.github.com>
2022-12-01 10:33:12 +0100

    file update

--- File.java
+++ File.java
@@ -1,3 +1,46 @@
-public class File
+import java.util.*;
+import java.io.*;
+import java.awt.Desktop;
+
+public class ScannerDemo1
 {
+
+
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        boolean sentinel = true;
+        Scanner scan = new Scanner(System.in);
+        Desktop desktop = Desktop.getDesktop();
+        File fMembers = new File("members.txt");
+
+        while (sentinel)
+        {
+            System.out.println("Do you want to see the members? y/n");
+            char c = scan.next().charAt(0);
+            if (c == 'y')
+            {
+                sentinel = false;
+
+                try
+                {
+                    desktop.open(fMembers);
+                }
+                catch (Exception e)
+                {
+                    System.out.println(e);
+                }
+            }
+            else if (c == 'n')
+            {
+                System.out.println("");
+                sentinel = false;
+                scan.close();
+            }
+            else
+            {
+                System.out.println("Wrong input, try again please");
+            }
+
+        }
+    }
 }
